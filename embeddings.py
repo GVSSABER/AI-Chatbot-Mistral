ade5fd76d85f3ecbdcb5f438753475bdc4646960
GVSSABER <garlapativenkatasaisathvik@gmail.com>
2026-05-06 13:56:14 +0530

    Delete embeddings.py

--- embeddings.py
+++
@@ -1,7 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-# FORCE CPU ONLY (IMPORTANT)
-model = SentenceTransformer("all-MiniLM-L6-v2", device="cpu")
-
-def get_embeddings(texts):
-    return model.encode(texts, convert_to_numpy=True)
